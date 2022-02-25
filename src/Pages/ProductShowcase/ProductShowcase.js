f1e141bf0d0f64139bdb1b77d4001dec5ddcfee2
Imane Elouazzani <denjala.smiley@gmail.com>
2022-02-25 21:19:33 +0100

    ajout des produits

--- src/Pages/ProductShowcase/ProductShowcase.js
+++ src/Pages/ProductShowcase/ProductShowcase.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import inventory from "../../data/inventory.js";
 import {useParams} from 'react-router-dom'
 import './ProductShowcase.css'
@@ -13,6 +13,47 @@ export default function ProductShowcase() {
   const productClicked = inventory.findIndex(
     (obj) => obj.title.replace(/\s+/g, "").trim() === id
   );
+
+  const dispatch = useDispatch()
+  
+  const addingInfo = useRef()
+
+  let timerInfo
+  let display = true
+
+  const updateMugs = (e) => {
+      setNbMugs(Number(e.target.value))
+  }
+
+  const addToCart = e => {
+    e.preventDefault()
+
+    const itemAdded = {
+      ...inventory[productClicked],
+      quantity: nbMugs
+    }
+
+    dispatch({
+      type: "ADDITEM",
+      payload: itemAdded
+    })
+
+    addingInfo.current.innerText = "Ajouté au panier"
+
+    if(display){
+      display = false
+      timerInfo = setTimeout(() => {
+        addingInfo.current.innerText = ""
+        display = true
+      }, 500)
+    }
+  }
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timerInfo)
+    }
+  }, [])
 
   return (
     <div className="showcase">
@@ -29,17 +70,19 @@ export default function ProductShowcase() {
       <div className="product-infos">
         <h2>{inventory[productClicked].title}</h2>
         <p>Prix: {inventory[productClicked].price}€</p>
-        <form >
+        <form 
+          onSubmit={addToCart}
+        >
           <label htmlFor="quantity">Quantité</label>
           <input
             type="number"
             id="quanitity"
             value={nbMugs}
-           
+            onChange={updateMugs}
           />
           <button>Ajouter au panier</button>
           <span 
-          
+          ref={addingInfo}
           className="adding-info"></span>
         </form>
       </div>
